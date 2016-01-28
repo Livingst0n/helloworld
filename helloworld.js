872fb4dec5053cf78b6415280c7e0b5223044d90
Livingst0n <mikelivingston04@outlook.com>
2016-01-28 11:23:17 -0700

    Change back to jquery

--- helloworld.js
+++ helloworld.js
@@ -1,15 +1,15 @@
-﻿/*$(document).ready(function () {
+﻿$(document).ready(function () {
     jQuery('#clickme').on('click', function () {
         jQuery('#display').text("Hello worldlings!");
     });
-});*/
+});
 
-function thisFunction() {
+/*function thisFunction() {
     document.getElementById('display').innerHTML = "Hello worldlings!";
     //.addEventListener('click', StartButtons, false);
 
     //document.getElementById('display').addEventListener('click', thisFunction, false);
-}
+}*/
 
 /*function onDeviceReady() {
     document.getElementById('display').addEventListener('click', thisFunction, false);
