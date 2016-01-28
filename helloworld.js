a7bf157804a92681f61676c088a50ac57a114be3
Livingst0n <mikelivingston04@outlook.com>
2016-01-28 09:42:55 -0700

    stuff

--- helloworld.js
+++ helloworld.js
@@ -6,4 +6,11 @@
 
 function thisFunction() {
     document.getElementById('display').innerHTML = "Hello worldlings!";
+    //.addEventListener('click', StartButtons, false);
+
+    //document.getElementById('display').addEventListener('click', thisFunction, false);
 }
+
+/*function onDeviceReady() {
+    document.getElementById('display').addEventListener('click', thisFunction, false);
+}*/
