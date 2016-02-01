3e24b32876ed5294ac6ce2b1cc8f24d4eddbffed
Livingst0n <mikelivingston04@outlook.com>
2016-02-01 09:50:05 -0700

    Add API calls

--- helloworld.js
+++ helloworld.js
@@ -2,6 +2,33 @@
     jQuery('#clickme').on('click', function () {
         jQuery('#display').text("Hello worldlings!");
     });
+
+    var url = 'http://pjc.gear.host/api/Hello';
+    $.getJSON(url)
+        .done(function (data) {
+            //stuff
+            $.each(data, function (key, item) {
+                //more stuff
+                $('<li>', { text: formatItem(item) }).appendTo($('#records'));
+            });
+        });
+
+    function formatItem(item) {
+        return item.helloID + ":" + item.helloLanguage + ":" + item.helloMessage + ".";
+    }
+
+
+    /*$.ajax({
+        url: url,
+        dataType: 'xml',
+        timeout: 5000,
+        success: function (data, status) {
+            
+        },
+        error: function () {
+            
+        }
+    });*/
 });
 
 /*function thisFunction() {
@@ -14,3 +41,6 @@
 /*function onDeviceReady() {
     document.getElementById('display').addEventListener('click', thisFunction, false);
 }*/
+
+
+//pjc.gear.host/api/Hello
