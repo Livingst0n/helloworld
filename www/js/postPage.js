dca7e7fcdc32d5d0f13bdd431f1fbd947b2ef6d6
Livingst0n <mikelivingston04@outlook.com>
2016-02-02 17:23:14 -0700

    add post files

---
+++ www/js/postPage.js
@@ -0,0 +1,66 @@
+//var app = {
+//    initialize: function() {
+//        document.addEventListener('deviceready', this.onDeviceReady, false);
+//    },
+
+//    onDeviceReady: function () {
+//        alert("hi");
+//        var url = 'http://pjc.gear.host/api/Hello';
+//    /*$.getJSON(url)
+//        .done(function (data) {
+//            //stuff
+//            $.each(data, function (key, item) {
+//                //more stuff
+//                $('<li>', { text: formatItem(item) }).appendTo($('#records'));
+//            });
+//        });*/
+//        $(document).ready(function () {
+//            alert("hello");
+//            var myLanguage = jQuery("#language").val();
+//            var myMessage = jQuery("#message").val();
+
+//            jQuery('#submitButton').on('click', function () {
+//                console.log("boom");
+//                $.post(url, { helloLanguage: myLanguage, helloMessage: myMessage })
+//                    .done(function (data) {
+//                        alert("Data Loaded: " + data);
+//                    });
+//                console.log("boom2");
+//            });
+//        });
+        
+        
+        
+        
+
+//        /*$.ajax({
+//            type: 'GET',
+//            url: url,
+//            dataType: 'json',
+//            success: function (data, status) {
+//                $.each(data, function (key, item) {
+//                    $('<li>', { text: formatItem(item) }).appendTo($('#records'));
+//                });
+//            },
+//            error: function () {
+//                //error loading data
+//            }
+//        });*/
+
+//        /*function formatItem(item) {
+//            return item.helloID + ":" + item.helloLanguage + ":" + item.helloMessage + ".";
+//        }*/
+//    }
+//};
+
+///*$(document).ready(function () {
+//    //$.support.cors = true;
+//    //$.mobile.allowCrossDomainPages = true;
+    
+//});*/
+
+//app.initialize();
+
+jQuery(document).ready(function () {
+    alert("hi");
+});
